c5908a3e39448370f3e3a7cfb341cfb023dd451f
Akhil Doddi <akhild042015@gmail.com>
2025-03-20 19:22:24 -0500

    inital commit

---
+++ self_test_2.py
@@ -0,0 +1,24 @@
+"""This is a "self" example with cats. To run this, you will only need one thing:
+ Python.
+ If you have that, then you are ready to run this code. This code will teach you how to use classes, how to define, and use self the appropriate way."""
+
+
+
+class Cat:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def purr(self):
+        print(f"{self.name} - purr")
+
+    def change_name(self, new_name):
+        self.name = new_name
+
+cat = Cat("Spiral", "American Shorthair")
+
+cat.purr()
+
+cat.change_name("Clusky")
+
+cat.purr()
